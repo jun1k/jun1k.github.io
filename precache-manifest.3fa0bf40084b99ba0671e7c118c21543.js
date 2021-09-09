543f03dd74461ceec36dac5dc84f1a05a84c23ea
jun1k <jun1k@gmail.com>
2021-09-09 18:29:44 +0900

    Updates

---
+++ precache-manifest.3fa0bf40084b99ba0671e7c118c21543.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb571c9403582f9fc28519dd6f17508",
+    "url": "/index.html"
+  },
+  {
+    "revision": "360d4db3cdbe14e83f2f",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "c68bb3be6ef43f203e94",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "23f3f693f1aab84c5b24",
+    "url": "/static/css/main.509cd038.chunk.css"
+  },
+  {
+    "revision": "4db49c094c31c64dfdec",
+    "url": "/static/js/2.8e875ff9.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.8e875ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360d4db3cdbe14e83f2f",
+    "url": "/static/js/3.e6defe08.chunk.js"
+  },
+  {
+    "revision": "c68bb3be6ef43f203e94",
+    "url": "/static/js/4.1b2609e7.chunk.js"
+  },
+  {
+    "revision": "23f3f693f1aab84c5b24",
+    "url": "/static/js/main.7dfed298.chunk.js"
+  },
+  {
+    "revision": "e0ccaf9b1b53800268a9",
+    "url": "/static/js/runtime-main.44dc22d0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "556235b9c80005593a39e9f25ba48c5c",
+    "url": "/static/media/oddoneLogo.556235b9.png"
+  },
+  {
+    "revision": "7853d2fa162673235d529ae62b85a76c",
+    "url": "/static/media/opic.7853d2fa.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "9c9a60a5a0c96357b3a420ea5f51f7de",
+    "url": "/static/media/skkuLogo.9c9a60a5.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "24fb3db86c9e26cd661d15c60c5f2289",
+    "url": "/static/media/ustbLogo.24fb3db8.png"
+  }
+]);
